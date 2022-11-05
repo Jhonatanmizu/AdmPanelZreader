783ea1725767e049f5a32f660f4a746c66a8a80e
Jhonatanmizu <natanjesuss20@gmail.com>
2022-11-04 23:56:50 -0300

    :sparkles: finished publications layout

---
+++ src/components/ButtonWithIcon/ButtonWithIcon.vue
@@ -0,0 +1,70 @@
+<template>
+    <button :class="['icon-btn', {'edit-btn':  edit   }]" @click="handleClick">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+import { reactive, toRefs } from 'vue'
+
+export default {
+    props: {
+        handle: {
+            type: Function,
+            required: true,
+            default() {
+                console.log("ICON BTN");
+            }
+        },
+        edit:Boolean
+    },
+    setup(props) {
+        const state = reactive({
+            count: 0,
+        })
+        const handleClick = () => {
+            props.handle()
+        }
+        console.log("ICON BTN", props.handle);
+        return {
+            ...toRefs(state),
+            handleClick
+        }
+    }
+}
+</script>
+
+<style scoped>
+.icon-btn {
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    outline: none;
+    color: var(--secondary-color);
+    background-color: var(--primary-color);
+    box-shadow: 0 0.3rem rgba(121, 121, 121, 0.65);
+    border-radius: 0.625rem;
+    font-size: 1.5rem;
+    cursor: pointer;
+
+}
+
+.icon-btn:active {
+    transform: translate(0, 0.3rem);
+    box-shadow: 0 0.1rem rgba(255, 255, 255, 0.65);
+}
+
+.icon-btn:disabled {
+    opacity: 0.7;
+}
+
+.icon-btn:hover {
+    filter: brightness(110%);
+}
+.edit-btn{
+    background-color: var(--green-color);
+
+}
+</style>
--- src/components/ScientificPublications/ScientficPublications.vue
+++ src/components/ScientificPublications/ScientficPublications.vue
@@ -1,58 +1,152 @@
 <template>
   <div class="container p">
-    <form class="form" @submit="handleFormSubmit">
-      <div class="form__controls">
-        <div class="input__group">
-          <label for="text" class="input-label">Texto</label>
-          <input type="text" class="input-text" v-model="formData.text" />
+    <div class="row-add">
+      <ButtonWithIcon :handle="() => toggleIsVisible()">
+        <i class="fa-solid fa-plus"></i>
+
+      </ButtonWithIcon>
+    </div>
+    <Transition name="fade">
+      <form class="form" @submit="handleFormSubmit" v-if="isVisible">
+        <div class="form__controls">
+          <div class="input__group">
+            <input type="hidden" name="id" v-model="formData.id">
+          </div>
+          <div class="input__group">
+            <label for="text" class="input-label">Texto</label>
+            <input type="text" class="input-text" v-model="formData.text" />
+          </div>
+          <div class="input__group">
+            <label for="link" class="input-label">link</label>
+            <input type="text" class="input-text" v-model="formData.link" />
+          </div>
         </div>
-        <div class="input__group">
-          <label for="link" class="input-label">link</label>
-          <input type="text" class="input-text" v-model="formData.link" />
+        <div class="form__actions">
+          <Button text="Cancelar" type="button" @click="clearFormData" />
+          <Button text="Salvar" type="submit" action="save" :isDisabled="checkFormData()" />
         </div>
+      </form>
+    </Transition>
+    <Transition name="fade">
+      <div class="publications__container" v-if="!isVisible">
+        <ul class="publications">
+          <li class="publication" v-for="publication in publications" :key="publication">
+            {{ publication.text }}
+            <div class="publication__actions">
+
+              <ButtonWithIcon edit :handle="() => editPublication(publication.id)">
+                <i class="fa-solid fa-pen-to-square"></i>
+
+              </ButtonWithIcon>
+              <ButtonWithIcon :handle="() => deletePublication(publication.id)">
+                <i class="fa-solid fa-trash"></i>
+              </ButtonWithIcon>
+            </div>
+          </li>
+        </ul>
+        <!-- <div class="publication" v-for="publication in publications" :key="publication"> -->
+
+        <!-- </div> -->
       </div>
-      <div class="form__actions">
-        <Button text="Cancelar" />
-        <Button
-          text="Salvar"
-          type="submit"
-          action="save"
-          :isDisabled="checkFormData()"
-        />
-      </div>
-    </form>
+    </Transition>
+
   </div>
 </template>
 
 <script>
-import { reactive } from "vue";
+import { ref } from "vue";
 import Button from "../Button/Button.vue";
+import ButtonWithIcon from "../ButtonWithIcon/ButtonWithIcon.vue";
 
 export default {
   setup() {
-    const formData = reactive({
+    const publications = ref([
+      {
+        id: 1,
+        text: "alog",
+        link: "google.com",
+      }
+    ])
+    const formData = ref({
+      id: null,
       text: "",
       link: "",
     });
+    const isVisible = ref(false)
+    const toggleIsVisible = () => {
+      console.log("TOGGLE");
+      isVisible.value = !isVisible.value
+      console.log("ISVISIBLE", isVisible.value);
+    }
+    const logData = () => {
+      console.log(publications);
+    }
     const handleFormSubmit = (e) => {
       e.preventDefault();
-      console.log("FORM DATA", formData);
+      console.log("FORM DATA", formData.value);
+      let model = {
+        ...formData.value,
+        id: publications.value.length
+      }
+      if (model)
+        publications.value.push(model)
+      clearFormData()
     };
+    const getPublications = () => {
+      for (let i = 0; i < 3; i++) {
+        publications.value.push()
+
+      }
+    }
+    const editPublication = (id) => {
+      toggleIsVisible()
+      let found = publications.value.find(p => p.id === id)
+      formData.value = {
+        id: found.id,
+        text: found.text,
+        link: found.link
+      }
+      console.log("FOUND", found);
+      console.log("FORM DATA", formData.value);
+
+    }
+    const deletePublication = (id) => {
+      // TODO Lógica cabulosa
+      console.log("DELETANDO", id);
+      publications.value = publications.value.filter(p => p.id != id)
+      console.log("PUBLICATIONS", publications.value);
+      getPublications()
+
+    }
     const checkFormData = () => {
-      if (!formData.text || !formData.link) {
+      if (!formData.value.text || !formData.value.link) {
         return true;
       } else {
         return false;
       }
     };
+    const clearFormData = () => {
+      formData.value = {
+        id: null,
+        text: null,
+        link: null
+      }
+      toggleIsVisible()
+    }
     return {
-      //   ...toRefs(state),
       formData,
       handleFormSubmit,
       checkFormData,
+      publications,
+      logData,
+      deletePublication,
+      editPublication,
+      clearFormData,
+      isVisible,
+      toggleIsVisible
     };
   },
-  components: { Button },
+  components: { Button, ButtonWithIcon },
 };
 </script>
 
@@ -64,31 +158,94 @@ export default {
   /* justify-content: space-between; */
   width: 100%;
 }
+
 .form__controls {
   display: flex;
   align-items: center;
   justify-content: space-between;
   width: 100%;
 }
+
 .container {
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 }
+
 .form__actions {
   margin-top: 1rem;
   display: flex;
   align-items: center;
   justify-content: flex-end;
   width: 100%;
+  gap: 2rem
 }
+
+.publications__container {
+  width: 100%;
+  margin-top: .2rem 0;
+
+}
+
+.publications {
+  display: flex;
+  width: 100%;
+  align-items: center;
+  flex-direction: column;
+  justify-content: center;
+  gap: .3rem;
+}
+
+.publications .publication {
+  background-color: var(--alt-color);
+  padding: .5rem;
+  border-radius: .5rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  font-size: 2rem;
+  box-shadow: 0 0.3rem rgba(121, 121, 121, 0.65);
+
+}
+
+.publication .publication__actions {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: .2rem;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.row-add {
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  margin: 1rem 0;
+  width: 100%;
+}
+
 @media screen and (max-width: 800px) {
   .form__controls {
     flex-direction: column;
   }
+
   .form__actions {
-    justify-content: space-between;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    /* justify-content: space-between; */
   }
+
 }
 </style>
